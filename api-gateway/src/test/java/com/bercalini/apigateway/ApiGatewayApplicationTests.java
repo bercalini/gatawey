5e1f3b521e199bf0a10c4a2de49a63705cf52385
bercalinidev <alisson.develooper@hotmail.com]>
2022-04-27 13:47:30 -0300

    api gatawey

---
+++ api-gateway/src/test/java/com/bercalini/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bercalini.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
